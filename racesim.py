966b6f99d9802116e83dfc8b7501570e3bb9fdfd
James Hawkins <james@hawkinsplace.co.uk>
2021-03-28 13:02:01 +0100

    added types to racesim

--- racesim.py
+++ racesim.py
@@ -21,13 +21,15 @@ def get_cpu_context():
     return cl.Context(devices=my_gpu_devices)
 
 class TrackParams(object):
-    def __init__(self, length, width, clean_air_dist):
+    def __init__(self, length: int, width: int, clean_air_dist: int):
         self.length = length
         self.width = width
         self.clean_air_dist = clean_air_dist
 
 class CompetetorParams(object):
-    def __init__(self, n_competetors: int, track_conditions, track_preferences, dist_params, resp_levels, resp_durations):
+    def __init__(self, n_competetors: int, track_conditions: np.array(np.float32),
+                 track_preferences: np.array(np.float32), dist_params: np.array(np.float32),
+                 resp_levels: np.array(np.float32), resp_durations: np.array(np.float32)):
         self.n_competetors = n_competetors
         mag = np.sqrt(len(track_conditions))
 
@@ -40,7 +42,7 @@ class CompetetorParams(object):
         self.resp_durations = resp_durations
 
 class RaceSim(object):
-    def __init__(self, context, n_races, track_params, competetor_params):
+    def __init__(self, context: cl.Context, n_races: int, track_params: TrackParams, competetor_params: CompetetorParams):
         self.__context = context
         self.__n_races = n_races
         self._track_params = track_params
@@ -78,7 +80,7 @@ class RaceSim(object):
 
         self.offset = int(time())
 
-    def __build_program(self, context):
+    def __build_program(self, context: cl.Context) -> cl.Program:
         '''
         Load kernel and build program for chosen device (gpu/cpu)
         Returns resultant program
@@ -91,14 +93,14 @@ class RaceSim(object):
         options = "-D n_c=%d -D n_r=%d -D l=%d -D w=%f -D clean_air_dist=%d" % (self._competetor_params.n_competetors, self.__n_races, self._track_params.length, self._track_params.width, self._track_params.clean_air_dist)
         return cl.Program(context, kernelsource).build(options)
 
-    def __format_positions(self, positions):
+    def __format_positions(self, positions: np.array(np.float32)) -> np.array(np.float32):
         '''
         Allocate memory for competetor positions using
         provided positions for each race instance
         '''
         return np.tile(positions, (self.__n_races, 1)).astype(np.float32)
 
-    def set_competetor_positions(self, competetor_positions):
+    def set_competetor_positions(self, competetor_positions: np.array(np.float32)) -> None:
         self._h_positions = self.__format_positions(competetor_positions)
         self._h_winners = np.zeros(self.__n_races).astype(np.int8)
         mf = cl.mem_flags
@@ -106,7 +108,7 @@ class RaceSim(object):
         self.__d_tmp_positions = cl.Buffer(self.__context, mf.COPY_HOST_PTR, hostbuf=self._h_positions) # Read and write
         self._d_winners = cl.Buffer(self.__context, mf.COPY_HOST_PTR, hostbuf=self._h_winners) # Read and write
 
-    def _step(self, n_steps):
+    def _step(self, n_steps: int) -> None:
         '''Complete 'n_steps' of the simulation'''
         for i in range(n_steps):
             self.offset += 2*self.__n_positions
@@ -117,17 +119,17 @@ class RaceSim(object):
             self.update_positions(self._queue, (self.__n_races, self._competetor_params.n_competetors), None,
                 self.__d_preferences, self.__d_rngs, self.__d_resp_levels, self.__d_resp_durations, self.__d_tmp_positions, self._d_positions, self._d_winners, self.offset)
 
-    def _stop(self):
+    def _stop(self) -> None:
         '''Wait for the queue to finish so data may be transferred from the device'''
         self._queue.finish()
 
 
 class RaceSimSerial(RaceSim):
-    def __init__(self, track_params, competetor_params):
+    def __init__(self, track_params: TrackParams, competetor_params: CompetetorParams):
         context = get_cpu_context()
         super().__init__(context, 1, track_params, competetor_params)
 
-    def get_competetor_positions(self):
+    def get_competetor_positions(self) -> np.array(np.float32):
         '''
         Copy competetor positions from the device
         Returns the copied competetor positions
@@ -136,18 +138,18 @@ class RaceSimSerial(RaceSim):
         cl.enqueue_copy(self._queue, self._h_positions, self._d_positions)
         return self._h_positions[0]
 
-    def step(self, n_steps):
+    def step(self, n_steps) -> None:
         '''Complete 'n_steps' of the simulation'''
         self._step(n_steps)
 
 
 class RaceSimParallel(RaceSim):
-    def __init__(self, max_steps, n_races, track_params, competetor_params):
+    def __init__(self, max_steps: int, n_races: int, track_params: TrackParams, competetor_params: CompetetorParams):
         self.max_steps = max_steps
         context = get_gpu_context()
         super().__init__(context, n_races, track_params, competetor_params)
 
-    def __get_steps_remaining(self, max_steps, competetor_positions, track_length) -> int:
+    def __get_steps_remaining(self, max_steps: int, competetor_positions: np.array(np.float32), track_length: int) -> int:
         '''
         Calculate and return how many more steps of the simulation
         need to take place before all races have finished
@@ -156,7 +158,7 @@ class RaceSimParallel(RaceSim):
         percent_complete = min_pos / track_length
         return int(max_steps * (1.0 - percent_complete)) // 2
 
-    def simulate_races(self, competetor_positions):
+    def simulate_races(self, competetor_positions: np.array(np.float32)) -> np.array(np.int8):
         '''
         Run the racing simulation with competetors starting
         from the positions defined in 'competetor_positions'
@@ -174,9 +176,9 @@ class RaceSimParallel(RaceSim):
         rtime = time() - rtime
         print("The kernel ran in", rtime, "seconds")
 
-        return self.get_winners()
+        return self.__get_winners()
 
-    def get_winners(self):
+    def __get_winners(self) -> np.array(np.int8):
         '''
         Copy the race winners from the device
         Returns the copied winners
